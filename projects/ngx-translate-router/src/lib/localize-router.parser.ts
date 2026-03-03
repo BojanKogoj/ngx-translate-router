76c32f4b82852503156598f119bd80ada6a40b60
Bojan <bojan.kogoj@geneplanet.com>
2026-03-03 10:02:24 +0100

    Replaced deprecated ngx-translate/core method

--- projects/ngx-translate-router/src/lib/localize-router.parser.ts
+++ projects/ngx-translate-router/src/lib/localize-router.parser.ts
@@ -78,7 +78,7 @@ export abstract class LocalizeParser {
       this.defaultLang = this._cachedLang || browserLang || this.locales[0];
     }
     selectedLanguage = locationLang || this.defaultLang;
-    this.translate.setDefaultLang(this.defaultLang);
+    this.translate.setFallbackLang(this.defaultLang);
 
     let children: Routes = [];
     /** if set prefix is enforced */
